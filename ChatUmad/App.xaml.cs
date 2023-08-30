a622abe19c375b94afebe2263b5e6d53ea0b4be6
Emigdio <millosespinosa@gmail.com>
2023-08-30 16:00:20 -0600

    Add project files.

---
+++ ChatUmad/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ChatUmad
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ChatUmad/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ChatUmad
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
