df6bcbf6f27f84edc5779cfbac48b822dd7d267f
Millos63 <millosespinosa@gmail.com>
2023-09-14 14:36:07 -0500

    cambios

--- ChatUmad/App.xaml.cs
+++ ChatUmad/App.xaml.cs
@@ -8,7 +8,7 @@ namespace ChatUmad
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new MainPage());
+            MainPage = new NavigationPage(new Chat());
         }
     }
 }
