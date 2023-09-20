57ec917c31df2aafa791733ffc70594c767a23d9
Millos63 <millosespinosa@gmail.com>
2023-09-20 11:23:55 -0600

    Nuevo commit modificando main

--- ChatUmad/MainPage.xaml.cs
+++ ChatUmad/MainPage.xaml.cs
@@ -14,13 +14,10 @@
         {
             
             await Navigation.PushAsync(new Paginas.Chat());
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            
             //await Navigation.PushAsync(new Paginas.UserList());
         }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
-
-            //await Navigation.PushAsync(new Paginas.UserList());
         }
     }
 }
