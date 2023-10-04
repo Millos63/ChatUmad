5d53eb06343af6b47824164bc283bb11bb41405a
job561.corte@gmail.com <job_t2000@hotmail.com>
2023-10-04 11:25:27 -0600

    Campio de tamaño de letra

--- ChatUmad/Paginas/avisos.xaml.cs
+++ ChatUmad/Paginas/avisos.xaml.cs
@@ -1,5 +1,8 @@
 
 using ChatUmad.Data;
+using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+using System.Threading;
 
 namespace ChatUmad;
 
@@ -11,9 +14,15 @@ public partial class avisos : ContentPage
         InitializeComponent();
         Avisos = new List<Aviso1>
         {
-            new Aviso1{Pro="Coordinador", Avisop="El dia 16 de Septiempre se realizará un simulacro a las 11:00am" }
+            new Aviso1{Pro="Coordinador", Avisop="El dia 16 de Septiempre se realizará un simulacro a las 11:00am" },
+            new Aviso1{Pro="Coordinador", Avisop="Hola a todos" }
         };
         this.BindingContext = this;
+    }
+    async void ShowToastButtonClicked(object? sender, EventArgs args)
+    {
+        var toast = Toast.Make("Se a publicado");
+        await toast.Show();
     }
 
     private void OnCounterClicked1(object sender, EventArgs e)
